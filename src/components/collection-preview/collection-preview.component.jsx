639d907f8fa00a5780229505a56a979b749986c3
Talon Durrant <talonmdurrant@gmail.com>
2021-01-23 05:44:46 -0700

    using apollo to add a new item to the cart dropdown

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react"
+import { withRouter } from "react-router-dom"
 
-import CollectionItem from '../collection-item/collection-item.component';
+import { default as CollectionItem } from "../collection-item/collection-item.container"
 
-import './collection-preview.styles.scss';
+import "./collection-preview.styles.scss"
 
 const CollectionPreview = ({ title, items, history, match }) => (
-  <div className='collection-preview'>
-    <h1
-      className='title'
-      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
-    >
+  <div className="collection-preview">
+    <h1 className="title" onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}>
       {title.toUpperCase()}
     </h1>
-    <div className='preview'>
+    <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
+        .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
-);
+)
 
-export default withRouter(CollectionPreview);
+export default withRouter(CollectionPreview)
